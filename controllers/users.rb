381cc8ea7fe9becb3dec80e1ce88d3467f872c8d
Ahmad Amireh <ahmad.amireh@gmail.com>
2012-07-30 08:09:23 +0300

    Added support for colour schemes ... profile editing now provides a live preview of editor settings, and set up default preferences

--- controllers/users.rb
+++ controllers/users.rb
@@ -1,5 +1,30 @@
 require 'json'
 
+class Hash
+
+  # Merges self with another hash, recursively.
+  # 
+  # This code was lovingly stolen from some random gem:
+  # http://gemjack.com/gems/tartan-0.1.1/classes/Hash.html
+  # 
+  # Thanks to whoever made it.
+
+  def deep_merge(hash)
+    target = dup
+    
+    hash.keys.each do |key|
+      if hash[key].is_a? Hash and self[key].is_a? Hash
+        target[key] = target[key].deep_merge(hash[key])
+        next
+      end
+      
+      target[key] = hash[key]
+    end
+    
+    target
+  end
+end
+
 helpers do
   def logged_in?
     session[:authorized]
@@ -24,7 +49,8 @@ helpers do
 
   def preferences
     @preferences ||= JSON.parse(current_user.settings || "{}")
-    @preferences
+    settings.default_preferences.deep_merge(@preferences)
+    # set_defaults(settings.default_preferences, @preferences)
   end
 
 end
@@ -85,6 +111,8 @@ get '/logout' do
 end
 
 get '/profile' do
+  restricted!
+  
   erb :"/users/edit"
 end
 
@@ -135,7 +163,7 @@ end
 
 post '/profile/password' do
   restricted!
-  
+
   pw = Digest::SHA1.hexdigest(params[:password][:current])
 
   if current_user.password == pw then
